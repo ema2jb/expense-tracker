fae5a4f964f10b0099cb07c8eabfd2e99f7183bc
ema2jb <ema2jb@gmail.com>
2021-06-17 22:20:25 +0100

    first version of site

---
+++ src/features/budgets/budgetsSlice.js
@@ -0,0 +1,57 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 
+'healthcare', 'personal', 'education', 'entertainment'];
+const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
+
+
+
+const budgetSlice = createSlice({
+    name: 'budgets',
+    initialState: initialState,
+    reducers: {
+        editBudget: (state, action)=>{
+          const newBudgets = state.map(budget => {
+            if (budget.category === action.payload.category) {
+              return action.payload;
+            }
+            return budget;
+          })
+          return newBudgets;     
+        } 
+    }
+
+})
+
+export const selectBudgets = (state) => state.budgets;
+export const {editBudget} = budgetSlice.actions
+export default budgetSlice.reducer;
+
+
+
+/*export const editBudget = (budget) => {
+  return {
+    type: 'budgets/editBudget',
+    payload: budget
+  }
+}
+
+const budgetsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'budgets/editBudget':
+      const newBudgets = state.map(budget => {
+        if (budget.category === action.payload.category) {
+          return action.payload;
+        }
+        return budget;
+      })
+      return newBudgets;
+    default:
+      return state;
+  }
+}*/
+
+
+
